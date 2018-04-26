0b6b830b5ccc23f21bf71fddb9ebd160d2bf478c
panjamapon karnasuta <happyudongstudio@gmail.com>
2018-04-27 02:16:10 +0700

    nextflix ios app clone

--- Screens/TabsView.js
+++ Screens/TabsView.js
@@ -24,13 +24,81 @@ import { Container,
   FooterTab,
   Footer,
   Button,
+  H1, H2, H3, H4, H5, H6,
   Input } from 'native-base';
 import { TabNavigator } from "react-navigation";
 import React_Icon from 'react-native-vector-icons';
+
+let mainSlider = 
+[
+    {
+        "id": "10233",
+        "name": "13 reasons why",
+        "img_url": "https://lhsstatesman.com/wp-content/uploads/2017/05/13-reasons-why.jpg"
+    },
+    {
+        "id": "202321",
+        "name": "narcos",
+        "img_url": "http://www.hoodedutilitarian.com/wp-content/uploads/2015/09/narcos0004.jpg"
+    },
+    {
+        "id": "402324",
+        "name": "house of cards",
+        "img_url": "https://vignette.wikia.nocookie.net/house-of-cards/images/a/a8/House_of_Cards_Season_1_Poster.jpg/revision/latest?cb=20140217231358"
+    },
+    {
+        "id": "502324",
+        "name": "dark",
+        "img_url": "http://fr.web.img4.acsta.net/pictures/17/11/10/12/18/2448823.jpg"
+    },
+    {
+        "id": "6028724",
+        "name": "resident evil",
+        "img_url": "http://cdn.cinepop.com.br/2018/03/residentevilserie.jpg"
+    }
+]
+
+let feed = [
+    {
+      "id": "7028724",
+      "name": "resident evil",
+      "img_url": "http://cdn.cinepop.com.br/2018/03/residentevilserie.jpg"
+    },
+    {
+      "id": "90233",
+      "name": "rick and morty",
+      "img_url": "https://imgc.allpostersimages.com/img/print/posters/rick-morty-cast-of-emotions_a-G-14537695-0.jpg"
+    },
+    {
+      "id": "80233",
+      "name": "13 reasons why",
+      "img_url": "https://lhsstatesman.com/wp-content/uploads/2017/05/13-reasons-why.jpg"
+    }
+]
+
+let action = [
+  {
+    "id": "11128724",
+    "name": "the matrix",
+    "img_url": "http://4.bp.blogspot.com/-QWXQ5UydcZY/T_nJNkPbpQI/AAAAAAAAFRM/rpew1lKMciY/s1600/the_matrix_-_poster_1__1999_.jpg"
+  },
+  {
+    "id": "1342128724",
+    "name": "the matrix reload",
+    "img_url": "https://occ-0-999-1001.1.nflxso.net/art/907c8/f8c9b526241fc592533ff1aa2d533ed08a2907c8.jpg"
+  }
+]
 class FirstScreen extends Component {
     static navigationOptions = {
       header: null,
     };
+
+    _keyExtractor = (item, index) => item.id;
+    _renderItem(item) {
+      return (
+        <Image style={{width:220, height:330, overflow:'hidden'}} source={{uri:item.img_url}}/>
+      )
+    }
     render() {
       return (
         <View style={styles.container}>
@@ -40,9 +108,35 @@ class FirstScreen extends Component {
                   <Image style={styles.logo} source={require('../assets/images/netflix.png')} />
               </Body>
             </Header>
-            <Container>
-                <Text style={styles.text}>hello</Text>
-            </Container>
+            <ScrollView>
+               <FlatList
+                horizontal
+                keyExtractor={this._keyExtractor}
+                renderItem={({item}) => this._renderItem(item)} 
+                data={mainSlider}
+               />
+              <H3 style={styles.headingTitle}>
+                Series & Movie Update
+              </H3>
+              <FlatList
+                  horizontal
+                  keyExtractor={this._keyExtractor}
+                  ItemSeparatorComponent={ () => <View style={{ width: 20}}/> }
+                  renderItem={({item}) => this._renderItem(item)} 
+                  data={feed}
+                />
+
+              <H3 style={styles.headingTitle}>
+                Action Sci-Fi
+              </H3>
+              <FlatList
+                  horizontal
+                  keyExtractor={this._keyExtractor}
+                  ItemSeparatorComponent={ () => <View style={{ width: 20}}/> }
+                  renderItem={({item}) => this._renderItem(item)} 
+                  data={action}
+              />
+            </ScrollView>
         </View>
       )
     }
@@ -144,7 +238,7 @@ const styles = StyleSheet.create({
       width: undefined,
       height: undefined,
       justifyContent: 'center',
-      backgroundColor:'#141414'
+      backgroundColor:'#000'
     },
     containerLogo: {
       backgroundColor:'#fff',
@@ -158,5 +252,11 @@ const styles = StyleSheet.create({
     },
     Icon: {
       color:'#a4aab3'
+    },
+    headingTitle: {
+      padding:20,
+      fontSize:16,
+      fontWeight:'bold',
+      color:'#fff'
     }
 });
